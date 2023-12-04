d28a7d76558713a0069a3d31166c5b7030312df6
Raphael Borba <raphaelbsx@gmail.com>
2023-12-04 17:41:42 -0300

    css

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -21,11 +21,11 @@ export default function ProjectCard({ data }: IProps) {
                 height={600}
                 className="rounded-xl group-hover:opacity-10"
             />
-            <div className="absolute left-[50%] top-[50%] hidden translate-x-[-50%] translate-y-[-50%] group-hover:block">
+            <div className="project__card__info__box group-hover:block">
                 <h3 className="text-center text-2xl tracking-wider text-white">{data.title}</h3>
                 <p className="pb-4 pt-2 text-center text-white">{data.stack}</p>
                 <Link href="/">
-                    <p className="cursor-pointer rounded-lg bg-white py-3 text-center text-lg font-bold text-gray-700">More Info</p>
+                    <p className="project__card__info__button">More Info</p>
                 </Link>
             </div>
         </div>
