1c4e2e75ed2b05c590f6c138980d39d3d0abfbd5
Raphael Borba <raphaelbsx@gmail.com>
2023-12-06 22:21:33 -0300

    add: match labels

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,11 +1,11 @@
 "use client"
 import { handleScroll } from "@/utils";
 import Image from "next/image";
-import Link from "next/link";
 import { AiOutlineMail } from "react-icons/ai";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 import { HiOutlineChevronDoubleUp } from "react-icons/hi";
+import contactImage from '../../public/contact.jpg'
 
 
 
@@ -23,9 +23,8 @@ export default function Contact() {
                             <div>
                                 <Image
                                     alt="contact"
-                                    src="/contact.jpg"
-                                    width={400}
-                                    height={400}
+                                    src={contactImage}
+
                                     className="rounded-xl duration-200 ease-in hover:scale-105"
                                 />
                             </div>
@@ -60,25 +59,25 @@ export default function Contact() {
                             <form >
                                 <div className="grid w-full gap-4 py-2 md:grid-cols-2">
                                     <div className="flex flex-col">
-                                        <label htmlFor="" className="py-2 text-sm uppercase">Name</label>
-                                        <input type="text" className="flex rounded-lg border-2 border-gray-300 p-3" />
+                                        <label htmlFor="name" className="py-2 text-sm uppercase">Name</label>
+                                        <input type="text" id="name" className="flex rounded-lg border-2 border-gray-300 p-3" />
                                     </div>
                                     <div className="flex flex-col">
-                                        <label htmlFor="" className="py-2 text-sm uppercase">Phone Number</label>
-                                        <input type="text" className="flex rounded-lg border-2 border-gray-300 p-3" />
+                                        <label htmlFor="phone" className="py-2 text-sm uppercase">Phone Number</label>
+                                        <input type="text" id="phone" className="flex rounded-lg border-2 border-gray-300 p-3" />
                                     </div>
                                 </div>
                                 <div className="flex flex-col">
-                                    <label htmlFor="" className="py-2 text-sm uppercase">Email</label>
-                                    <input type="email" className="flex rounded-lg border-2 border-gray-300 p-3" />
+                                    <label htmlFor="email" className="py-2 text-sm uppercase">Email</label>
+                                    <input type="email" id="email" className="flex rounded-lg border-2 border-gray-300 p-3" />
                                 </div>
                                 <div className="flex flex-col">
-                                    <label htmlFor="" className="py-2 text-sm uppercase">subject</label>
-                                    <input type="text" className="flex rounded-lg border-2 border-gray-300 p-3" />
+                                    <label htmlFor="subject" className="py-2 text-sm uppercase">subject</label>
+                                    <input type="text" id="subject" className="flex rounded-lg border-2 border-gray-300 p-3" />
                                 </div>
                                 <div className="flex flex-col">
-                                    <label htmlFor="" className="py-2 text-sm uppercase">Message</label>
-                                    <textarea rows={10} className="rounded-lg border-2 border-gray-300 p-3" />
+                                    <label htmlFor="message" className="py-2 text-sm uppercase">Message</label>
+                                    <textarea rows={10} id="message" className="rounded-lg border-2 border-gray-300 p-3" />
                                 </div>
                                 <button className="mt-4 w-full p-4 text-gray-100">Send Message</button>
                             </form>
